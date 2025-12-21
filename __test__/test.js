361fbafce1a5e64bb3ab3e184432b2603c9300fd
MelanyPerezUCE <melagatita1801@gmail.com>
2025-12-21 18:36:49 -0500

    first commit

---
+++ __test__/test.js
@@ -0,0 +1,13 @@
+import { sumar, multiplicar } from '../math.js';
+
+describe('Tests matemáticos', () => {
+
+  test('la suma NO debe ser mayor a 5', () => {
+    expect(sumar(3, 3)).toBeLessThanOrEqual(5);
+  });
+
+  test('la multiplicación debe ser mayor o igual a 10', () => {
+    expect(multiplicar(5, 1)).toBeGreaterThanOrEqual(10);
+  });
+
+});
