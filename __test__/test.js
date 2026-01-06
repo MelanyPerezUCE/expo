6b7882ccdaed18d34b73148329ece83f40783af1
MelanyPerezUCE <melagatita1801@gmail.com>
2026-01-06 14:59:01 -0500

    test: :test_tube: forzar fallo en prueba

--- __test__/test.js
+++ __test__/test.js
@@ -3,11 +3,11 @@ import { sumar, multiplicar } from '../math.js';
 describe('Tests matemáticos', () => {
 
   test('la suma NO debe ser mayor a 5', () => {
-    expect(sumar(3, 1)).toBeLessThanOrEqual(5);
+    expect(sumar(9, 9)).toBeLessThanOrEqual(5);
   });
 
   test('la multiplicación debe ser mayor o igual a 10', () => {
-    expect(multiplicar(5, 2)).toBeGreaterThanOrEqual(10);
+    expect(multiplicar(1, 1)).toBeGreaterThanOrEqual(10);
   });
 
 });
